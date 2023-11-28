703b90b574e2fcf40e3120b96b1a076e3e9d07fb
sajeel12 <msajeelahmad2001@gmail.com>
2023-11-28 15:30:59 +0500

    s

--- sum_up/settings.py
+++ sum_up/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s7ii89qqejpxx@n5f3a&^8g7-nks@kld3=eypxbzls_lmmj@az'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['18.133.137.12']
+ALLOWED_HOSTS = ['18.133.137.12', '127.0.0.1']
 
 
 # Application definition
@@ -119,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,4 @@
 import os
 from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sum_up.settings')
 application = get_wsgi_application()
